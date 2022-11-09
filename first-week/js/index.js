aa6de72cc8f49158ed3877f3a5d9d44a3a917ba3
José Nogueira Neto <jnognet@gmail.com>
2022-11-08 23:37:34 -0500

    minha solução e suas reescritas da arrow function apresentada

--- first-week/js/index.js
+++ first-week/js/index.js
@@ -297,6 +297,14 @@ function out(){
 const testArrow = () => ((x=7), ()=>(x+5))  ()
 console.log(testArrow())
 
+// outra forma passo a passo da função acima
+
+// const arrayfunc = () => { let x = 7; y = (x) => x+5; return y(x) }
+// const arrayfunc = () => { y = (x) => x+5; return y(7) }
+// const arrayfunc = () => { return ((x = 7) => x+5)(); }
+const arrayfunc = () => ((x = 7) => x+5)()
+console.log(arrayfunc())
+
 const namesLength = names.map( ({length}) => length )
 console.log(namesLength)
 
