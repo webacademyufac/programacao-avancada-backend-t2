d07557b63700d1ee6032a019b328334f15d175bf
Victor Antunes Vieira <victor.vieira@ifac.edu.br>
2022-11-10 19:48:49 -0500

    códigos da aula de 10 de novembro

--- node-day-01/scripts.js
+++ node-day-01/scripts.js
@@ -17,8 +17,8 @@ server.listen(port, hostname, () => {
 
 // callback
 
+/*
 const fs = require('fs')
-
 console.log('Primeiro comando.')
 fs.readFile('./text.txt',callback)
 function callback(err,content){
@@ -30,5 +30,104 @@ console.log('Terceiro comando.')
 
 // utilizando programação assíncrona
 setTimeout(() => (console.log('testando o setTimeout')), 3000)
-
 console.log('comando que está após setTimeout')
+ */
+// utilizando programação síncrona
+
+/* 
+function sum(){
+  const result = 1+1
+  if(result === 2){
+    success()
+  }else{
+    erro()
+  }
+}
+function success(){
+  console.log('Sucesso! A soma é 2.')
+}
+function erro(){
+  console.log('A soma não é 2. Alguma coisa deu errado...')
+}
+
+sum()
+console.log('testando exibição...')
+ */
+
+// transformando em callback (programação assíncrona)
+
+/* 
+function sum(callback, callbackError){
+  const result = 1+1
+  if(result === 2){
+    callback()
+  }else{
+    callbackError()
+  }
+}
+
+sum(
+  () => (console.log('Sucesso! A soma é 2.')),
+  () => (console.log('A soma não é 2. Alguma coisa deu errado...'))
+)
+console.log('testando exibição...')
+ */
+
+// Promise - é uma classe; a tradução significa promessa
+// then()
+// resolve e reject
+
+const promise = new Promise((resolve,reject)=> {
+  const result = 1+2
+  if(result === 2){
+    resolve('Sucesso! A soma é 2.')
+  }else{
+    reject('A soma não é 2. Alguma coisa deu errado...')
+  }
+})
+
+promise.then(
+  ((resultado)=>(console.log(resultado))),
+  ((erro)=>(console.log(erro)))
+)
+
+// voltando para calback
+
+/* 
+const name = 'Paulo Sampaio'
+
+function whoIsTheBest(callback,callbackError){
+  if(name != 'Paulo Sampaio'){
+    callbackError('Tá errado. Não tem como!')
+  }else{
+    callback({
+      name: name,
+      message: `humildemente o melhor`
+    })
+  }
+}
+whoIsTheBest( 
+  ((success)=>(console.log(`${success.name} é ${success.message}.`))) ,
+  ((error)=>(console.log(error))) 
+)
+ */
+
+// transformando em Promise
+
+const name = 'Paulo Sampaio'
+
+const p = new Promise((resolve, reject) => {
+  if(name != 'Paulo Sampaio'){
+    reject('Tá errado. Não tem como!')
+  }else{
+    resolve({
+      name: name,
+      message: `humildemente o melhor`
+    })
+  }
+})
+
+p.then( 
+  ((success)=>(console.log(`${success.name} é ${success.message}.`))) ,
+  ((error)=>(console.log(error))) 
+)
