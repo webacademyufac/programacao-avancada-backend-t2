078d06f5caf09506ed3256e16531e96113e1ae50
Victor Antunes Vieira <victor.vieira@ifac.edu.br>
2022-11-14 20:50:14 -0500

    códigos da aula de 14 de novembro

--- project-favorites/server.js
+++ project-favorites/server.js
@@ -4,18 +4,32 @@ const fs = require('fs')
 
 http.createServer((req, res)=>{
     
-    // definindo rotas para a aplicação
-    if(req.url === '/')
-        fs.readFile(
-            path.join(__dirname,'public','index.html'),
-            (err, content) => {
-                if (err) throw err
-                res.end(content)
-            }
-        )
 
-
-    if(req.url === '/contato')
-        res.end('<h1>Contato</h1>')
     
+
+    const file = req.url === '/' ? 'index.html' : req.url
+    const filePath = path.join(__dirname,'public',file)
+    
+    // pegando a extensão do documento
+    const extname = path.extname(filePath)
+
+    // criando um vetor com as extensões permitidas
+    const allowedFileTypes = ['.html', '.js', '.css']
+
+    
+    const allowed = allowedFileTypes.find(
+        (item) => item === extname 
+    )
+
+    if(!allowed) return
+
+    // definindo rotas para a aplicação
+    fs.readFile(
+        filePath,
+        (err, content) => {
+            if (err) throw err
+            res.end(content)
+        }
+    )
+
 }).listen(5000, () => (console.log('Servidor rodando...')))
