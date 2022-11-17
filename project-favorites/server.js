73db028306672987ac4c03b5a7f3e4de13d42608
vrcvieira <victor.vieira@ifac.edu.br>
2022-11-17 11:23:23 -0500

    códigos da aula de 14 de novembro

--- project-favorites/server.js
+++ project-favorites/server.js
@@ -4,26 +4,25 @@ const fs = require('fs')
 
 http.createServer((req, res)=>{
     
-
-    
-
+    // as rotas da aplicação foram definidas de maneira dinâmica
     const file = req.url === '/' ? 'index.html' : req.url
     const filePath = path.join(__dirname,'public',file)
     
-    // pegando a extensão do documento
+    // pegando a extensão do arquivo
     const extname = path.extname(filePath)
 
-    // criando um vetor com as extensões permitidas
+    // criando um vetor com as extensões esperadas
     const allowedFileTypes = ['.html', '.js', '.css']
 
-    
+    // varrendo o vetor de extensões permitidas para tentar encontrar algum arquivo com extensão diferente das esperadas 
     const allowed = allowedFileTypes.find(
         (item) => item === extname 
     )
 
+    // se encontrou uma extensão não esperada, retorna, somente para não travar a aplicação
     if(!allowed) return
 
-    // definindo rotas para a aplicação
+
     fs.readFile(
         filePath,
         (err, content) => {
