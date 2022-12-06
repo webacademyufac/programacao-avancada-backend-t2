9031799d457fa694c19d841b58413238c748318e
biangomes <biangomes0707@gmail.com>
2022-12-05 20:05:48 -0500

    QueryString adicionada como comentário

--- project-favorites/api/index.js
+++ project-favorites/api/index.js
@@ -3,6 +3,11 @@ const data = require('./urls.json');
 const URL = require('url');
 
 http.createServer((req, res) => {
+    /*
+        GET: http://localhost:3000/
+        CREATE: http://localhost:3000/?name=linkedin&url=https://www.linkedin.com
+        DELETE: http://localhost:3000/?name=linkedin&url=https://www.linkedin.com/&del=1
+    */
     const { name, url, del } = URL.parse(req.url, true).query
     
     if(!name || !url) {
