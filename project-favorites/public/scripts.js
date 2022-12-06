9fdcce9c110430b73e3adaf8bb4c06583042478e
Victor Antunes Vieira <victor.vieira@ifac.edu.br>
2022-12-05 20:20:31 -0500

    códigos da aula de 05 de dezembro

--- project-favorites/public/scripts.js
+++ project-favorites/public/scripts.js
@@ -1,7 +1,7 @@
 const ul = document.querySelector('ul')
 const input = document.querySelector('input')
 const form = document.querySelector('form')
-
+/* 
 // Função que carrega o conteúdo da API.
 async function load() {
     // fetch está como await para evitar que entre num esquema de promisse e só devolva o conteúdo após a iteração qua acontece em seguida.
@@ -12,7 +12,7 @@ async function load() {
 }
 
 load()
-
+ */
 function addElement({ name, url }) {
     const li = document.createElement('li')
     const a = document.createElement("a")
