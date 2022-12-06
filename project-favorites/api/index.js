e529e95c0728fd29a5409bded9c307897e364136
biangomes <biangomes0707@gmail.com>
2022-12-05 20:04:05 -0500

    Teste de API: url?name=algum_nome&url=alguma_url

--- project-favorites/api/index.js
+++ project-favorites/api/index.js
@@ -6,13 +6,10 @@ http.createServer((req, res) => {
     const { name, url, del } = URL.parse(req.url, true).query
     
     if(!name || !url) {
-        console.log(name, url, del)
         return res.end('show')
     }
     if(del) {
-        console.log(name, url, del)
         return res.end('delete')
     }
-    console.log(name, url, del)
     return res.end('create')
 }).listen(3000, () => {console.log("API is running")});
