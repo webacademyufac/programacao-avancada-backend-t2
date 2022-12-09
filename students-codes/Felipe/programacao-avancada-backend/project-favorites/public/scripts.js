9502fd2f832d5a9bb8a41a743c241f80b52a5bb9
Felipegadelha <felipegadelha062@gmail.com>
2022-12-09 20:01:54 -0300

    Desafios e Projetos

---
+++ students-codes/Felipe/programacao-avancada-backend/project-favorites/public/scripts.js
@@ -0,0 +1,63 @@
+const ul = document.querySelector('ul')
+const input = document.querySelector('input')
+const form = document.querySelector('form')
+
+
+
+async function load(){
+    const res = await fetch('http://localhost:3000/').then(data => data.json())
+    res.urls.map(({name,url}) => addElement({name,url}))
+}
+load();
+
+function addElement({ name, url }) {
+    const li = document.createElement('li')
+    const a = document.createElement("a")
+    const trash = document.createElement("span")
+
+    a.href = url
+    a.innerHTML = name
+    a.target = "_blank"
+
+    trash.innerHTML = "x"
+    trash.onclick = () => removeElement(trash)
+
+    li.append(a)
+    li.append(trash)
+    ul.append(li)
+}
+
+function removeElement(el) {
+   
+    const linkElement = el.parentNode.children[0]
+    const name = linkElement.innerText, url = linkElement.getAttribute('href')
+    if (confirm('Tem certeza que deseja deletar?')){
+        el.parentNode.remove()
+        fetch(`http://localhost:3000?name=${name}&url=${url}&del=delete`)
+    }
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    let { value } = input
+
+    if (!value)
+        return alert('Preencha o campo!')
+
+    const [name, url] = value.split(',')
+
+    if (!url)
+        return alert('O texto não está formatado da maneira correta.')
+
+    if (!/^http/.test(url))
+        return alert('Digite a url da maneira correta.')
+
+    // addElement({ name, url })
+
+    fetch(`http://localhost:3000?name=${name}&url=${url}`)
+    ul.innerHTML=''
+    load();
+    input.value = ''
+})
+
